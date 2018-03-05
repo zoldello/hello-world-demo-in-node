b0f458b676784782523c58d2e5b80b033e2ac291
Phil <zoldello@gmail.com>
2018-03-05 03:13:17 -0600

    updated test

--- spec/routes/message.spec.js
+++ spec/routes/message.spec.js
@@ -1,20 +1,42 @@
-//const message = require('../../routes/message');
-//const sinon = require('sinon');
-//const chai = require('chai');
-//const expect = chai.expect;
-//const express = require('express');
-//const router = express.Router();
-//const MessageDataService = require('../../data/message');
-//const endpoint = 'welcome';
-//
-//describe('Test of data service', function() {
-//	it('should return an expected message', function() {
-//	    message.get();
-//	});
-//});
+const message = require('../../routes/message');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const express = require('express');
+const router = express.Router();
+const MessageDataService = require('../../data/message');
+const endpoint = 'welcome';
+
+describe('Test of data service', function() {
+	afterEach(() => {
+		sinon.restore();
+	});
+
+	it('should return an expected message', function() {
+		const reqStub = sinon.stub();
+		const resStub = sinon.stub();
+		const statusStub = sinon.stub();
+		const jsonStub = sinon.stub();
+		const testInput = 'abc';
+		const MessageDataServiceStub =
+            sinon
+            	.stub(MessageDataService.prototype, 'welcome')
+            	.returns(testInput);
+
+		statusStub.returns(jsonStub);
+		resStub.returns(statusStub);
+
+		resStub.returns();
+
+	   message.get('welcome', function(reqStub, resStub) {});
+
+		// expect(resStub.callCount).toBe(1);
+	});
+});
 
 ///////////////
 /*
+
 const express = require('express');
 const MessageDataService = require('../data/message');
 
@@ -24,7 +46,7 @@ router.get('/welcome', (req, res, next) => {
     const messageDataService = new MessageDataService();
     let welcomeData = messageDataService.welcome();
 
-    res.json(welcomeData);
+    res.status(200).json(welcomeData);
 });
 
 module.exports = router;
