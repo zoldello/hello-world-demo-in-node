2ba072b77eaa64275a7b34e3b0e0e4644c217184
Phil <zoldello@gmail.com>
2018-03-05 13:27:39 -0600

    commented out messge test

--- spec/routes/message.spec.js
+++ spec/routes/message.spec.js
@@ -1,38 +1,42 @@
-const message = require('../../routes/message');
-const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
-const express = require('express');
-const router = express.Router();
-const MessageDataService = require('../../data/message');
-const endpoint = 'welcome';
-
-describe('Test of data service', function() {
-	afterEach(() => {
-		sinon.restore();
-	});
-
-	it('should return an expected message', function() {
-		const reqStub = sinon.stub();
-		const resStub = sinon.stub();
-		const statusStub = sinon.stub();
-		const jsonStub = sinon.stub();
-		const testInput = 'abc';
-		const MessageDataServiceStub =
-            sinon
-            	.stub(MessageDataService.prototype, 'welcome')
-            	.returns(testInput);
-
-		statusStub.returns(jsonStub);
-		resStub.returns(statusStub);
-
-		resStub.returns();
-
-	   message.get('welcome', function(reqStub, resStub) {});
-
-		// expect(resStub.callCount).toBe(1);
-	});
-});
+//const messageRouter = require('../../routes/message');
+//const sinon = require('sinon');
+//const express = require('express');
+//const router = express.Router();
+//const MessageDataService = require('../../data/message');
+//const endpoint = 'welcome';
+//const request = require('supertest');
+//
+//describe('Test of data service', function() {
+//	afterEach(() => {
+//		//sinon.restore();
+//	});
+//
+//	it('should return an expected message', function() {
+//		const reqStub = sinon.stub();
+//		const resStub = sinon.stub();
+//		const statusStub = sinon.stub();
+//		const jsonStub = sinon.stub();
+//		const testInput = 'abc';
+//		const MessageDataServiceStub =
+//            sinon
+//            	.stub(MessageDataService.prototype, 'welcome')
+//            	.returns(testInput);
+//
+//		statusStub.returns(jsonStub);
+//		resStub.returns(statusStub);
+//
+//		resStub.returns();
+//
+//	  messageRouter['/welcome'].fn
+//
+//
+//        console.log(jsonStub.calledOnce);
+//        console.log('--------------------');
+//        expect(true).toBe(true);
+//        //expect(jsonStub.calledOnce).toBe(true);
+//		// expect(resStub.callCount).toBe(1);
+//	});
+//});
 
 ///////////////
 /*
